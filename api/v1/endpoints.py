9531056cb1ce7b3f630051f24f2c321a494e9ade
aohus <dbtnghk528@gmail.com>
2024-10-28 15:14:43 +0900

    fix: seperate html file

--- api/v1/endpoints.py
+++ api/v1/endpoints.py
@@ -1,7 +1,8 @@
-from fastapi import WebSocket, APIRouter
+from fastapi import WebSocket, APIRouter, Request
 from app.service.chat_service import ChatService
 from app.adapters.token_service import TokenService
 from app.adapters.pubsub_service import PubSubService
+from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 import logging
 
@@ -10,63 +11,10 @@ logger = logging.getLogger("uvicorn")
 chat_router = APIRouter()
 
 
-html = """
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Chat</title>
-    </head>
-    <body>
-        <h1>WebSocket Chat</h1>
-        <h2>Your ID: <span id="ws-id"></span></h2>
-        <form action="" onsubmit="sendMessage(event)">
-            <input type="text" id="messageText" autocomplete="off"/>
-            <button>Send</button>
-        </form>
-        <ul id='messages'>
-        </ul>
-        <script>
-            var client_id = Date.now();
-            document.querySelector("#ws-id").textContent = client_id;
-            var ws = new WebSocket(`ws://localhost:8000/api/v1/ws`);
-
-            ws.onmessage = function(event) {
-                var messages = document.getElementById('messages');
-                var messageData = JSON.parse(event.data);
-
-                var message = document.createElement('li');
-                var content = document.createTextNode(`${messageData.sender.id}: ${messageData.content}`);
-                message.appendChild(content);
-                messages.appendChild(message);
-            };
-
-            function sendMessage(event) {
-                var input = document.getElementById("messageText");
-                
-                // 메시지 데이터 구조를 작성
-                var messageData = {
-                    "type": "send_message",
-                    "content": input.value,           // 입력된 메시지 내용
-                    "sender": {
-                        "id": client_id,              // 클라이언트 ID
-                    },
-                    "room_id": "room1",               // 메시지가 전송될 방의 ID
-                    "timestamp": new Date().toISOString()  // 현재 시간 (ISO 8601 형식)
-                };
-
-                // JSON 문자열로 변환하여 전송
-                ws.send(JSON.stringify(messageData));
-                input.value = '';  // 입력 필드 초기화
-                event.preventDefault();  // 기본 폼 제출 방지
-            }
-        </script>
-    </body>
-</html>
-"""
-
-@chat_router.get("/")
-async def get():
-    return HTMLResponse(html)
+templates = Jinja2Templates(directory="templates")
+@chat_router.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse("chat.html", {"request": request})
 
 
 # 의존성 주입 설정
