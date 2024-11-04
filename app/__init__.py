efbeaf0c1967bce4d88dc3cf6c0a0270758ac253
aohus <dbtnghk528@gmail.com>
2024-11-04 22:27:06 +0900

    feature: monitoring setting

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
+from opentelemetry.instrumentation.asgi import OpenTelemetryMiddleware
 
 from api.v1 import router
+from core.otel_monitoring import setup_monitoring
+
+
+def init_monitoring() -> None:
+    setup_monitoring()
 
 
 def init_routers(app: FastAPI) -> None:
     app.include_router(router)
+
+
+def init_middleware(app: FastAPI) -> None:
+    app.add_middleware(OpenTelemetryMiddleware)
 
 
 def create_app() -> FastAPI:
@@ -13,7 +23,9 @@ def create_app() -> FastAPI:
         description="Live Chatting Server",
         version="1.0.0",
     )
+    init_monitoring()
     init_routers(app=app)
+    init_middleware(app=app)
     return app
 
 
