676c3c28bec717e392eabb3676549830c1431e30
aohus <dbtnghk528@gmail.com>
2024-11-15 13:17:41 +0900

    fix: pyinstrument middleware return response

--- chatservice/src/core/middleware/pyinstrument.py
+++ chatservice/src/core/middleware/pyinstrument.py
@@ -19,22 +19,25 @@ class ProfilerMiddleware:
             else None
         )
 
-        if scope["type"] == "websocket":
-            # WebSocket 요청 프로파일링 처리
-            if profiler:
-                profiler.start()  # 웹소켓 연결 시작 시 프로파일링 시작
+        if scope["type"] != "http":
+            await self.app(scope, receive, send)
+            return
 
-            async def receive_wrapper() -> Message:
-                message = await receive()
-                if message["type"] == "websocket.receive":
-                    if profiler:
-                        profiler.stop()
-                        print(profiler.output_text(unicode=True, color=True))
-                        profiler.start()  # 메시지 수신 후 프로파일링 재시작
-                elif message["type"] == "websocket.disconnect":
-                    if profiler:
-                        profiler.stop()
-                        print(profiler.output_text(unicode=True, color=True))
-                return message
+        # WebSocket 요청 프로파일링 처리
+        if profiler:
+            profiler.start()  # 웹소켓 연결 시작 시 프로파일링 시작
 
-            await self.app(scope, receive_wrapper, send)
+        async def receive_wrapper() -> Message:
+            message = await receive()
+            if message["type"] == "websocket.receive":
+                if profiler:
+                    profiler.stop()
+                    print(profiler.output_text(unicode=True, color=True))
+                    profiler.start()  # 메시지 수신 후 프로파일링 재시작
+            elif message["type"] == "websocket.disconnect":
+                if profiler:
+                    profiler.stop()
+                    print(profiler.output_text(unicode=True, color=True))
+            return message
+
+        await self.app(scope, receive_wrapper, send)
