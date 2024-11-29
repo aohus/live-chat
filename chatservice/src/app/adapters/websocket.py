a05c1004814e5f7d294f89d7dc39e16b55eaff88
aohus <dbtnghk528@gmail.com>
2024-11-29 11:00:30 +0900

    fix: remove close

--- chatservice/src/app/adapters/websocket.py
+++ chatservice/src/app/adapters/websocket.py
@@ -27,10 +27,8 @@ class WebSocketSession:
                 text = await self.websocket.receive_text()
                 yield text
         except WebSocketDisconnect:
-            await self.close()
             logging.info("WebSocket connection lost. Stopping receive_text task")
         except Exception as e:
-            await self.close()
             logging.error(f"Error in receive_text: {e}")
             raise
 
@@ -39,9 +37,7 @@ class WebSocketSession:
             logging.info(text)
             await self.websocket.send_text(text)
         except WebSocketDisconnect:
-            await self.close()
             logging.info("WebSocket connection lost. Stopping send_text task")
         except Exception as e:
-            await self.close()
             logging.error(f"Error in send_text: {e}")
             raise
